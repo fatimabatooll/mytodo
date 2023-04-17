d2f3a842954b7d7bd7727cad834b6e78714867e1
Fatima <124556936+fatimabatooll@users.noreply.github.com>
2023-04-18 02:53:29 +0500

    todo validation and red todo on previos date done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,76 +1,81 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import Button from './Button'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Button from "./Button";
 
-const Header = ({onSubmit, todo}) => {
-    
-    const [title, setTitle] = useState('')
-    const [detail, setDetail] = useState('')
-    const [date, setDate] = useState('')
-    
-    useEffect(() => {
-        if(todo){
-            setTitle(todo.title)
-            setDetail(detail.title)
-            setDate(date.title)
-        }
-    },[todo])
+const Header = ({ onSubmit, todo }) => {
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
+  const [date, setDate] = useState("");
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if(todo){
-            onSubmit(todo.id, title, detail, date)
-        } else {
-            onSubmit(null, title, detail, date)
-        }
-        setTitle('')
-        setDetail('')
-        setDate('')
+  useEffect(() => {
+    if (todo) {
+      setTitle(todo.title);
+      setDetail(todo.detail);
+      setDate(todo.date);
     }
+  }, [todo]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title == "") {
+      alert("title is required");
+    } else if (detail == "") {
+      alert("detail is required");
+    } else if (date == "") {
+      alert("date is required");
+    } else {
+      if (todo) {
+        onSubmit(todo.id, title, detail, date);
+      } else {
+        onSubmit(null, title, detail, date);
+      }
+      setTitle("");
+      setDetail("");
+      setDate("");
+    }
+  };
 
-    return (
+  return (
     <>
-    <Button />
-    <section className='article-div'>
-        <form className='form-div' onSubmit={handleSubmit}>
-            <div className='form-control'>
-                <h4>Title</h4>
-                <input 
-                type='text'
-                id='title'
-                className='todo'
-                name='title'
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                 />
-                 <h4> Details </h4>
-                 <input
-                 type='text'
-                 id='detail'
-                 className='todo'
-                 name='detail'
-                 value={detail}
-                 onChange={e => setDetail(e.target.value)}
-                 />
-                 <h4> Date </h4>
-                 <input 
-                 type='date'
-                 id='date'
-                 className='todo'
-                 name='date'
-                 value={date}
-                 onChange={e => setDate(e.target.value)}
-                 />
-            </div>
-            <button className='submit-btn' type="submit">{todo ? 'Save' : 'Add Todo'}</button>
+      <Button />
+      <section className="article-div">
+        <form className="form-div" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <h4>Title</h4>
+            <input
+              type="text"
+              id="title"
+              className="todo"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <h4> Details </h4>
+            <input
+              type="text"
+              id="detail"
+              className="todo"
+              name="detail"
+              value={detail}
+              onChange={(e) => setDetail(e.target.value)}
+            />
+            <h4> Date </h4>
+            <input
+              type="date"
+              id="date"
+              className="todo"
+              name="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <button className="submit-btn" type="submit">
+            {todo ? "Save" : "Add Todo"}
+          </button>
         </form>
-
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const TodoList = ({todos, onEdit, onDelete}) => {
+const TodoList = ({ todos, onEdit, onDelete }) => {
+  const currentDate = new Date();
   return (
-    <div className='todo-list'>
-        <ul>
-            {
-                todos.map(todo => (
-                    <article className='todo-item' key={todo.id}> 
-                    <h3>{todo.title}</h3>
-                    <p className='title'>{todo.detail}</p>
-                    <date className='title'>{todo.date}</date>
-                    <div className='btn-container'>
-               <button  className='edit-btn' onClick={() => onEdit(todo)}>Edit</button>
-               <button className='delete-btn' onClick={() => onDelete(todo.id)}>Del</button>
-          </div>
-                    </article>
-                ))
-            }
-        </ul>
+    <div className="todo-list ">
+      <ul>
+        {todos.map((todo) => {
+          const previousDate = new Date(todo.date);
+          const isActive = currentDate.getTime() > previousDate.getTime();
+          const articleClassName = `todo-item ${isActive ? "active" : ""}`;
+          return (
+            <article className={articleClassName} key={todo.id}>
+              <h3>{todo.title}</h3>
+              <p className="title">{todo.detail}</p>
+              <date className="title">{todo.date}</date>
+              <div className="btn-container">
+                <button className="edit-btn" onClick={() => onEdit(todo)}>
+                  Edit
+                </button>
+                <button
+                  className="delete-btn"
+                  onClick={() => onDelete(todo.id)}
+                >
+                  Del
+                </button>
+              </div>
+            </article>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
